11f6b441295a60770fe3701e086b59533aa6dfe5
switz-designs <switzdesignsstuff@gmail.com>
2020-09-25 12:30:57 -0700

    Updates

---
+++ precache-manifest.806246d1ed3212272aae77ec3baf0438.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51cd4d1afd6f64718510a8c3fc0abc0",
+    "url": "/civ6picker/index.html"
+  },
+  {
+    "revision": "8ce4d2a16f6cf6315f1e",
+    "url": "/civ6picker/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "ba31480893555b2b465f",
+    "url": "/civ6picker/static/css/main.451c5357.chunk.css"
+  },
+  {
+    "revision": "8ce4d2a16f6cf6315f1e",
+    "url": "/civ6picker/static/js/2.693d945a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/civ6picker/static/js/2.693d945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba31480893555b2b465f",
+    "url": "/civ6picker/static/js/main.53883957.chunk.js"
+  },
+  {
+    "revision": "08bacb5ef6eecca65981",
+    "url": "/civ6picker/static/js/runtime-main.3c6a5991.js"
+  },
+  {
+    "revision": "502542e8ed4b058c2055a785999cff6c",
+    "url": "/civ6picker/static/media/Alexander.502542e8.webp"
+  },
+  {
+    "revision": "3b8f70a4f20c24480a7b545bb19dc88c",
+    "url": "/civ6picker/static/media/Amanitore.3b8f70a4.webp"
+  },
+  {
+    "revision": "cf39146991c3dccc6fafd9ff1eb09912",
+    "url": "/civ6picker/static/media/Ambiorix.cf391469.webp"
+  },
+  {
+    "revision": "8796839f95275e01ffbb14f2dff11bc7",
+    "url": "/civ6picker/static/media/Basil_II.8796839f.webp"
+  },
+  {
+    "revision": "4c2d394afb004702f4eeae743d350068",
+    "url": "/civ6picker/static/media/Catherine_de_Medici_(Black Queen).4c2d394a.webp"
+  },
+  {
+    "revision": "680dbcaef97b8e97b85d3d6aa94164a3",
+    "url": "/civ6picker/static/media/Catherine_de_Medici_(Magnificence).680dbcae.webp"
+  },
+  {
+    "revision": "534a710d00fd7f835034cf3aececf9ad",
+    "url": "/civ6picker/static/media/Chandragupta.534a710d.webp"
+  },
+  {
+    "revision": "5bc3dea22166855883d7c205603012b9",
+    "url": "/civ6picker/static/media/Cleopatra.5bc3dea2.webp"
+  },
+  {
+    "revision": "c39d04e5e4d095d9b57a255c05a6ddc5",
+    "url": "/civ6picker/static/media/Cyrus.c39d04e5.webp"
+  },
+  {
+    "revision": "de43f92671ef44cd2c09bab8b9d76824",
+    "url": "/civ6picker/static/media/Dido.de43f926.webp"
+  },
+  {
+    "revision": "543a7144e6872d4cfed20f843798dc73",
+    "url": "/civ6picker/static/media/Eleanor_of_Aquitaine_(England).543a7144.webp"
+  },
+  {
+    "revision": "2c18d284427c10e7b932e305f6d60395",
+    "url": "/civ6picker/static/media/Eleanor_of_Aquitaine_(France).2c18d284.webp"
+  },
+  {
+    "revision": "06a7a00b96a220c3d1036e3dbd5cf254",
+    "url": "/civ6picker/static/media/Frederick_Barbarossa.06a7a00b.webp"
+  },
+  {
+    "revision": "47139d2923268f7db74394040afb29b3",
+    "url": "/civ6picker/static/media/Gandhi.47139d29.webp"
+  },
+  {
+    "revision": "da3538822e674aaa565b6d73d4a318bc",
+    "url": "/civ6picker/static/media/Genghis_Khan.da353882.webp"
+  },
+  {
+    "revision": "ad1c9bf9c0ef93c39e466284c6216658",
+    "url": "/civ6picker/static/media/Gilgamesh.ad1c9bf9.webp"
+  },
+  {
+    "revision": "3462aabb51466046c03e15a716e94db2",
+    "url": "/civ6picker/static/media/Gitarja.3462aabb.webp"
+  },
+  {
+    "revision": "7c90d9c45eed8965066c1b7d5fbec558",
+    "url": "/civ6picker/static/media/Gorgo.7c90d9c4.webp"
+  },
+  {
+    "revision": "1f4dec09ba0f3159e02e9df1eb92e3a1",
+    "url": "/civ6picker/static/media/Harald_Hardrada.1f4dec09.webp"
+  },
+  {
+    "revision": "62f3da2d9a4727729d51d16ff1bf0a05",
+    "url": "/civ6picker/static/media/Hojo_Tokimune.62f3da2d.webp"
+  },
+  {
+    "revision": "68dea13a87b107fff4ec3875194cf8cd",
+    "url": "/civ6picker/static/media/Jadwiga.68dea13a.webp"
+  },
+  {
+    "revision": "83e2683597dba6917076410a59e7a766",
+    "url": "/civ6picker/static/media/Jayavarman_VII.83e26835.webp"
+  },
+  {
+    "revision": "d77ddaec81f882835d31215210959656",
+    "url": "/civ6picker/static/media/John_Curtin.d77ddaec.webp"
+  },
+  {
+    "revision": "6ee019697cbd18d16063e46aee32512a",
+    "url": "/civ6picker/static/media/Kristina.6ee01969.webp"
+  },
+  {
+    "revision": "efb46166cbb100e18a8bc6cddee929f7",
+    "url": "/civ6picker/static/media/Kupe.efb46166.webp"
+  },
+  {
+    "revision": "c62581a04150c68ca4d047387dd89bb2",
+    "url": "/civ6picker/static/media/Lady_Six_Sky.c62581a0.webp"
+  },
+  {
+    "revision": "31640a38fec55e736d8dd6cb7dd66881",
+    "url": "/civ6picker/static/media/Lautaro.31640a38.webp"
+  },
+  {
+    "revision": "b6722ac95553f3063529b6a3c4bf1efd",
+    "url": "/civ6picker/static/media/Mansa_Musa.b6722ac9.webp"
+  },
+  {
+    "revision": "a880167e28eafe556d5e782d44753b7f",
+    "url": "/civ6picker/static/media/Matthias_Corvinus.a880167e.webp"
+  },
+  {
+    "revision": "c5f379935b360f53b43418ad769cb80c",
+    "url": "/civ6picker/static/media/Menelik_II.c5f37993.webp"
+  },
+  {
+    "revision": "20af19de440fe28a93ce95edb3bf2ca6",
+    "url": "/civ6picker/static/media/Montezuma.20af19de.webp"
+  },
+  {
+    "revision": "e0bf7445c871266c9f468fc502d7c1f5",
+    "url": "/civ6picker/static/media/Mvemba_a_Nzinga.e0bf7445.webp"
+  },
+  {
+    "revision": "ba7a297752ff433ebf12788cb69800a3",
+    "url": "/civ6picker/static/media/Pachacuti.ba7a2977.webp"
+  },
+  {
+    "revision": "3e1cffea5d40af5186e159055e8013bd",
+    "url": "/civ6picker/static/media/Pedro_II.3e1cffea.webp"
+  },
+  {
+    "revision": "6cfb4f79a89d83a25eac5e81ea6b0991",
+    "url": "/civ6picker/static/media/Pericles.6cfb4f79.webp"
+  },
+  {
+    "revision": "76d999783a83ae8c858fd65e735767fe",
+    "url": "/civ6picker/static/media/Peter.76d99978.webp"
+  },
+  {
+    "revision": "58ff2b366d259327129c68d2827b4a0c",
+    "url": "/civ6picker/static/media/Philip_II.58ff2b36.webp"
+  },
+  {
+    "revision": "c9a35a31b1c2122c1f5049933aaaa99d",
+    "url": "/civ6picker/static/media/Poundmaker.c9a35a31.webp"
+  },
+  {
+    "revision": "b1e5e5e48f56cc1d9a09fc5dafb11748",
+    "url": "/civ6picker/static/media/Qin_Shi_Huang.b1e5e5e4.webp"
+  },
+  {
+    "revision": "4ffd7fab9034332f7f82163c0295d300",
+    "url": "/civ6picker/static/media/Robert_the_Bruce.4ffd7fab.webp"
+  },
+  {
+    "revision": "c2e6cce79c9f91f29c8a72b0c047374f",
+    "url": "/civ6picker/static/media/Saladin.c2e6cce7.webp"
+  },
+  {
+    "revision": "f18cb98bd46b653e5e51c8f9cbe6207e",
+    "url": "/civ6picker/static/media/Seondeok.f18cb98b.webp"
+  },
+  {
+    "revision": "ec194345bcfd158073da08d584e673bb",
+    "url": "/civ6picker/static/media/Shaka.ec194345.webp"
+  },
+  {
+    "revision": "27ae9e62cc75838b2eec196a515cfe26",
+    "url": "/civ6picker/static/media/Simon_Bolivar.27ae9e62.webp"
+  },
+  {
+    "revision": "37a766468b2f505db48fe8e447850ff9",
+    "url": "/civ6picker/static/media/Suleiman.37a76646.webp"
+  },
+  {
+    "revision": "101cf20f7128472227060081ee07bb7f",
+    "url": "/civ6picker/static/media/Tamar.101cf20f.webp"
+  },
+  {
+    "revision": "96d4dd71b03eb2da296cdac040936c02",
+    "url": "/civ6picker/static/media/Teddy_Roosevelt_(Bull_Moose).96d4dd71.webp"
+  },
+  {
+    "revision": "6fd90be27466e57e985091879f7fe6a5",
+    "url": "/civ6picker/static/media/Teddy_Roosevelt_(Rough_Rider).6fd90be2.webp"
+  },
+  {
+    "revision": "50415f9092fb1316ac4fe3166e178e0e",
+    "url": "/civ6picker/static/media/Tomyris.50415f90.webp"
+  },
+  {
+    "revision": "063a40009f9ceec76645ece7e0ee7560",
+    "url": "/civ6picker/static/media/Trajan.063a4000.webp"
+  },
+  {
+    "revision": "b65a5050638c5696e9c3508ad84644da",
+    "url": "/civ6picker/static/media/Victoria.b65a5050.webp"
+  },
+  {
+    "revision": "7ca314c0e3e1498fdcfe4510771f94e0",
+    "url": "/civ6picker/static/media/Wilfrid_Laurier.7ca314c0.webp"
+  },
+  {
+    "revision": "10f999bcf582f2be0a7d6e47bb593b18",
+    "url": "/civ6picker/static/media/Wilhelmina.10f999bc.webp"
+  },
+  {
+    "revision": "343aed73cd82ea3d968e620a6cb2439b",
+    "url": "/civ6picker/static/media/bg1080p.343aed73.png"
+  }
+]);
